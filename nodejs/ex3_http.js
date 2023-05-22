51898802ed1d1c43e7d6e48ad3ee2e1a76a98740
jacktsr <caori888@gmail.com>
2023-05-22 09:24:10 +0000

    cleaning

--- nodejs/ex3_http.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-//create server
-http.createServer(function (req, res){
-  res.write('Hello world!'); // Write a response to client
-  res.end();  //end the response
-}).listen(8080);
--- nodejs/myfirst.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var dt = require('./myfirstmodule.js');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Current time is :" + dt.myDateTime());
-    res.end('');
-}).listen(8080);
